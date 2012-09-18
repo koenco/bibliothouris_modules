27bd010c763301b2fe305ea07ababfd89db3d0b0
Koen Colaers <koen.colaers@cegeka.be>
2012-09-18 14:56:19 +0200

    removed t('')

--- bibliotouris_book/autocomplete_autosubmit.js
+++ bibliotouris_book/autocomplete_autosubmit.js
@@ -3,5 +3,7 @@ Drupal.jsAC.prototype.select = function (node) {
   this.input.value = $(node).data('autocompleteValue');
   if(jQuery(this.input).hasClass('auto_submit')){
       this.input.form.submit();
+
+
   }};
 })(jQuery);   
