3e584761651604c16c3f597c0066e8cc8f5288b2
frankcr <frankcr@ubuntu.(none)>
2012-09-18 10:49:35 +0200

    Added users module

--- bibliotouris_book/autocomplete_autosubmit.js
+++ bibliotouris_book/autocomplete_autosubmit.js
@@ -2,6 +2,6 @@
 Drupal.jsAC.prototype.select = function (node) {
   this.input.value = $(node).data('autocompleteValue');
   if(jQuery(this.input).hasClass('auto_submit')){
-    this.input.form.submit();
+      this.input.form.submit();
   }};
 })(jQuery);   
