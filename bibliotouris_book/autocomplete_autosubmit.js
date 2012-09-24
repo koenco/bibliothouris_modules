f7166af5d2d57753c6c9ff4f497db313f7a0f748
koenc <koenc@ubuntu.(none)>
2012-09-24 11:43:10 +0200

    added some documentation

--- bibliotouris_book/autocomplete_autosubmit.js
+++ bibliotouris_book/autocomplete_autosubmit.js
@@ -2,9 +2,11 @@
 Drupal.jsAC.prototype.select = function (node) {
   this.input.value = $(node).data('autocompleteValue');
   if(jQuery(this.input).hasClass('auto_submit')){
+     // this submits the form and adds the values in the url, which breaks the form
      // this.input.form.submit();
 
-     $(this.input.form).find('.ctools-auto-submit-click').click();
+    // this triggers the correct autosubmit
+    $(this.input.form).find('.ctools-auto-submit-click').click();
 
   }
 };
